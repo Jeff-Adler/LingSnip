212d17750493c399d08104ec3931b04339f9f38b
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-22 14:33:09 -0500

    built user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,60 @@
 const mongoose = require('mongoose')
+const { default: validator } = require('validator')
 
 const userSchema = new mongoose.Schema({
-    name: {},
-    email: {},
-    password: {},
-    name: {},
-    age: {},
-    nativeLanguages: [],
-    otherLanguages: []
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error('Weak password')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    nativeLanguages: [{
+        language: {
+            type: String, 
+            trim: true,
+            lowercase: true,
+            required: true,
+            defaut: 'english'
+        }
+    }],
+    otherLanguages: [{
+        language:
+        {
+            type: String,
+            trim: true,
+            lowercase: true,
+        }
+    }]
 }, {
     timestamps: true
 })
