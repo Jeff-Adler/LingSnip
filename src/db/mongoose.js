5642462f68a1d8b8f70bb88f6957ac9abe0f49f2
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-22 18:36:44 -0500

    fixed dotenv file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,16 +1,7 @@
 const mongoose = require(`mongoose`)
 
-console.log(process.env.MONGODB_URL)
-const url = process.env.MONGODB_URL
-
-mongoose.connect("mongodb://127.0.0.1:27017/LingSnip-api", {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-
-// mongoose.connect(process.env.MONGODB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// })
