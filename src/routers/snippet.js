4154c5215e1862ff13915826ce35ac9b3a518cb7
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-22 21:07:14 -0500

    GET snippets path written

--- src/routers/snippet.js
+++ src/routers/snippet.js
@@ -17,6 +17,39 @@ router.post('/snippets', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?limit=(10)&skip=(0)
+// GET /tasks?sortBy=createdAt:(asc/desc)
+router.get('/snippets', auth, async (req, res) => {
+    // sort parameter
+    const sort = {}
+    
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        // parts[0] = field to sort by
+        // parts[1] = asc/desc
+        // sets sort value to -1 if query parameter is set to desc, 1 if asc/anything else
+        sort[parts[0]] = (parts[1] === 'desc' ? -1 : 1)
+    }
+    
+    try {
+        await req.user.populate({
+            // path specifies field on User for which we want to get data
+            path: 'snippets',
+            // specifies matching parameter for path. Shorthand notation for match: {}
+            options: {
+                //Mongoose knows to ignore this field if it's not passed a number, so default limit will essentially be 0
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                //Shorthand notation for sort : {} using sort variabe from above
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.snippets)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/snippets/:id', auth, async (req, res) => {
     const _id = req.params.id
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -50,7 +50,7 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 })
 
-router.get('/users/me', auth, async (req, res) => {
+router.get('/users/me/snippets', auth, async (req, res) => {
     res.send(req.user)
 })
 
